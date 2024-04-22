05cdc4c894827f1b24edd45f35ae136a7b6876fd
AndrewPerel <ap247@sunyorange.edu>
2024-04-21 21:48:07 -0400

    Summary

---
+++ RESCUE/scripts/script.js
@@ -0,0 +1,18 @@
+/*
+Andrew Perel
+4/21/2024
+script.js
+*/
+
+//Hamburger menu function
+function menu() {
+   var navlinks = document.getElementById("nav-links");
+   var menuicon = document.getElementById("icon");
+   if (navlinks.style.display === "block") {
+      navlinks.style.display = "none";
+      menuicon.style.color = "#2a1f14";
+   } else {
+      navlinks.style.display = "block";
+      menuicon.style.color = "#f6eee4";
+   }
+}
